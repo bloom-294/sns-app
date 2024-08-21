cb82a5c5c6ff3394bc7fa327f609be11ba4edb30
bloom-294 <non44921@gmail.com>
2024-08-21 09:31:50 +0900

    投稿機能追加

--- src/components/SideMenu.js
+++ src/components/SideMenu.js
@@ -1,7 +1,7 @@
 import { useContext } from 'react';
 import { SessionContext } from '../SessionProvider';
 
-export function SideMenu() {
+export const SideMenu = () => {
   const { currentUser } = useContext(SessionContext);
   return (
     <div className="bg-white p-4 rounded-lg shadow-md h-[200px] flex flex-col justify-center">
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,53 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { SessionContext } from "../SessionProvider";
 import { Navigate } from "react-router-dom";
 import { SideMenu } from "../components/SideMenu.js"
+import { postRepository } from "../repositories/post.js";
+import { Post } from "../components/Post.js";
+import { Pagination } from "../components/Pagination.js";
 
-function Home() {
+const limit = 5;
+
+const Home = () => {
+  const [content, setContent] = useState("");
+  const [posts, setPosts] = useState([]);
+  const [page, setPage] = useState(1);
   const { currentUser } = useContext(SessionContext);
+
+  const createPost = async() => {
+    const post = await postRepository.create(content, currentUser.id);
+    setPosts([
+      { ...post, userId: currentUser.id, userName: currentUser.userName },
+      ...posts,
+    ]);
+    setContent("");
+  }
+
+  const fetchPosts = async(page) => {
+    const posts = await postRepository.find(page, limit);
+    setPosts(posts);
+  }
+
+  const moveToNext = async() => {
+    const nextPage = page + 1;
+    await fetchPosts(nextPage);
+    setPage(nextPage);
+  }
+
+  const moveToPrev = async() => {
+    const prevPage = page - 1;
+    await fetchPosts(prevPage);
+    setPage(prevPage);
+  }
+
+  const deletePost = async(postId) => {
+    await postRepository.delete(postId);
+    setPosts(posts.filter((post) => post.id !== postId ))
+  }
+
+  useEffect(() => {
+    fetchPosts();
+  },[]);
 
   if(currentUser == null) return <Navigate replace to="/signin" />;
 
@@ -21,14 +64,27 @@ function Home() {
           <div className="col-span-2">
             <div className="bg-white p-4 rounded-lg shadow-md">
               <textarea
+                onChange={(e) => setContent(e.target.value)}
                 className="w-full p-2 mb-4 border-2 border-gray-200 rounded-md"
                 placeholder="What's on your mind?"
+                value={content}
               />
-              <button className="bg-[#726E68] text-white px-4 py-2 rounded-md hover:opacity-80 focus:outline-none  focus:opacity-50 disabled:opacity-50 disabled:cursor-not-allowed">
+              <button 
+                onClick={createPost}
+                className="bg-[#726E68] text-white px-4 py-2 rounded-md hover:opacity-80 focus:outline-none  focus:opacity-50 disabled:opacity-50 disabled:cursor-not-allowed"
+                disabled={content === ""}
+              >
                 Post
               </button>
             </div>
-            <div className="mt-4"></div>
+            <div className="mt-4">
+              {posts.map((post) => (
+                <Post key={post.id} post={post} onDelete={deletePost} />
+              ))}
+            </div>
+              <Pagination 
+              onPrev={page > 1 ? moveToPrev : null} 
+              onNext={posts.length >= limit ? moveToNext : null} />
           </div>
           <SideMenu />
         </div>
